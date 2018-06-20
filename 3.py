3920e02686c924a5a16d126054f76ad89ce45804
jpasner <jakepasner@gmail.com>
2018-06-19 19:32:12 -0700

    Finished project 3

--- 3.py
+++ 3.py
@@ -3,19 +3,46 @@
 #
 #     What is the largest prime factor of the number 600851475143 ?
 
-def find_factor(number)
-  
+# Solution: Recusive function which checks to see if input number has any factors and returns the largest prime factor it finds
 
-big_num = 600851475143
-half = 300425737572
-factors_list = []
-i = 1
+import math
 
+def is_factor(numerator,divisor):
+  if numerator % divisor is 0:
+    return 1
+  else:
+    return 0
 
+def find_prime_factors(number):
+  factors_list = []
+  i = 2
 
-while i < 300425737572:
-  if big_num % i is 0:
-    factors_list.append(i)
-  i += 1
+  upper_limit = number/2
 
-print factors_list
+  while i < upper_limit + 1:
+    if is_factor(number,i):
+      if find_prime_factors(i) > 0:
+        # i is a factor, but not prime.  Reset upper limit and continue searching
+        upper_limit = number / i
+      else:
+        # i is a prime factor, continue searching until upper limit is reached.
+        factors_list.append(i)
+    i += 1
+
+  if not factors_list:
+    # number is prime
+    return 0
+  else:
+    # return list of prime factors of number
+    return factors_list
+
+big_number = 600851475143
+
+output = find_prime_factors(big_number)
+
+if not output:
+  print str(big_number) + " is a prime number"
+else:
+  print "The prime factors of " + str(big_number) + " are:"
+  print output
+
